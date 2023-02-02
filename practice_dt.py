e32325518296a88431bcf90064f7ae32741e6bf7
punarsidhu13 <123929875+punarsidhu13@users.noreply.github.com>
2023-02-02 15:22:45 +0530

    Add files via upload

---
+++ practice_dt.py
@@ -0,0 +1,40 @@
+# int data type example 
+
+user_age = 22 
+print(user_age)
+
+
+# float data type example
+
+cgpa = 9.6
+print(cgpa)
+
+
+# tuple data type example
+
+week = ("monday","wednesday","sunday")
+print(week)
+
+
+# string data type example
+
+user_name= "Punar Pratap Singh Sidhu"
+print(user_name)
+
+
+# list data type example
+
+weekl = ["monday","wednesday","sunday"]
+print(weekl)
+
+
+# set data type example
+
+weeks = {"monday","wednesday","sunday"}
+print(weeks)
+
+
+# dictionary data type example
+
+capital_city = {"Nepal": "Kathmandu", "Italy": "Rome", "England": "London"}
+print(capital_city)
